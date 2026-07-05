e196c917a1275299437c579f494276f0cfeb1336
gensine <mishravaishnavi10d@gmail.com>
2026-07-06 00:19:31 +0530

    fix: truncate password to 72 bytes for bcrypt

--- api/auth_utils.py
+++ api/auth_utils.py
@@ -13,6 +13,7 @@ def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
 def get_password_hash(password):
+    password = password[:72]
     return pwd_context.hash(password)
 
 def create_access_token(data: dict):
